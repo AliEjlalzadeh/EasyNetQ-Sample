3fff612f2b355c72bfd7dd696b448763ed014325
Ali <Ali@DESKTOP-LG0PCFQ>
2021-06-03 00:04:31 +0430

    Add project files.

---
+++ Publisher/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using EasyNetQ;
+using Messages;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+                var input = String.Empty;
+                Console.WriteLine("Enter a message. 'Quit' to quit.");
+                int i = 1;
+                while (true)
+                {
+                    bus.PubSub.Publish(new TextMessage { Text = $"Message Number {i}" });
+                    Console.WriteLine($"Message {i} published!");
+                    i++;
+                }
+            }
+        }
+    }
+}
